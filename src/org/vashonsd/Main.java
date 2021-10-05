29a539b1fd0727a445091798ecbeb7b2960c86bf
mazur.spencer <mazur.spencer@vashonsd.org>
2021-10-05 11:01:21 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,18 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Comment added to force commit
+
+        System.out.println(squareOf(3));
+
+        System.out.println("helloWorld");
+
+
+    }
+
+    public static int squareOf(int num) {
+        return (num * num);
+    }
+}
